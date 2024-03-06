27709c45adba64ff9dca25ac13cd8441d591cca4
Ishan1231 <ishanmishra1231@gmail.com>
2024-03-05 22:23:56 -0500

    J4 Finished

--- 2024/J4TroublesomeKeys.py
+++ 2024/J4TroublesomeKeys.py
@@ -1,25 +1,35 @@
 A = list(input())
+
 B = list(input())
 
-actual_letter = ''
-silly_letter = ''
-silent_letter = ''
+silly_letter = ""
+silent_letter = ""
+actual_letter = ""
 
 for i in range(len(A)):
     try:
-        if A[i] == B[i]:
-            continue
-        elif A[i + 1] == B[i]:
-            B.insert(i, ' ')
+        if B[i] != A[i] and B[i] in A:
+            B.insert(i, " ")
+    except IndexError:
+        break
+
+for i in range(len(B)):
+    try:
+
+        if B[i] not in A and B[i] != " ":
+            actual_letter = A[i]
+            silly_letter = B[i]
+
+        elif B[i] == " ":
             silent_letter = A[i]
-        actual_letter = A[i]
-        silly_letter = B[i]
+
     except IndexError:
         silent_letter = A[i]
         break
-print(f"{A}\n{B}")
-print(f'{actual_letter} {silly_letter}')
+
+print(f"{actual_letter} {silly_letter}")
+
 if silent_letter == '':
-    print('-')
+    print("-")
 else:
     print(silent_letter)
